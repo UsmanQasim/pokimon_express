1b9ff7e68a37b70e4c78ad1d4b001d675f3fd01a
UsmanQasim <usman.qasim@whetstonez.com>
2024-06-03 11:08:35 +0500

    Added Zod Schema For Validation

--- src/middlewares/pokemon.middleware.ts
+++ src/middlewares/pokemon.middleware.ts
@@ -1,9 +1,26 @@
-// import { HookNextFunction } from "mongoose";
-// import { IPokemon } from "./models/pokimon.model";
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
 
-// export function updateTimestamps(this: IPokemon, next: HookNextFunction) {
-//   this.updated_at = new Date();
-//   next();
-// }
+import { StatusCodes } from "http-status-codes";
 
-// WILL USE IT LATER
+export function validateData(schema: z.ZodObject<any, any>) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: any) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
+        }));
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Invalid data", details: errorMessages });
+      } else {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}
